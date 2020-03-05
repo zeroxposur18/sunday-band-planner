df9c7fd299bca5efdd08007946ec39c2806b0b93
Tan Vu <vtan502262@gmail.com>
2020-03-05 14:55:00 -0600

    Code cleanup!

--- config/database.js
+++ config/database.js
@@ -7,14 +7,3 @@ mongoose.connect(process.env.DATABASE_URL, {
     useCreateIndex: true
 });
 
-
-// mongoose.connect('mongodb://localhost/movies',
-// { useNewUrlParser: true, useCreateIndex: true }
-// );
-
-// shortcut to mongoose.connection object
-// var db = mongoose.connection;
-
-// db.on('connected', function () {
-//   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-// });
--- controllers/comments.js
+++ controllers/comments.js
@@ -6,15 +6,10 @@ module.exports = {
 
 function create(req, res) {
     Band.findById(req.params.bandId, function(err, band) {
-        let review = band.reviews.id(req.params.reviewId);
-        console.log(review); 
-
-        console.log('req.body',req.body);          
+        let review = band.reviews.id(req.params.reviewId);         
         review.comment.push(req.body);
-
             band.save(function(err){
                 if (err) {
-                    console.log(err, band);
                     res.redirect(`/bands/`);
                 }
             else {res.redirect(`/bands/${band._id}`);}
